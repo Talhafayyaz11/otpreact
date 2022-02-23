1c24140e1319b9d0197d9aef085861f3175c4dd2
Talhafayyaz11 <talha@gems.techverx.com>
2022-02-24 01:44:17 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import "./App.css";
 
-function App() {
+function useOtp(setOtp,otp) {
+
+    useEffect(() => {
+     if ('OTPCredential' in window) {
+       const ac = new AbortController()
+       navigator.credentials
+         .get({
+           otp: { transport: ['sms'] },
+           signal: ac.signal,
+         })
+         .then((otp) => {
+           setOtp(otp.code)
+           ac.abort()
+         })
+         .catch((err) => {
+           ac.abort()
+           console.log(err)
+           setOtp(err)
+         })
+     }
+    }, [setOtp,otp])
+     return otp
+   }
+
+
+
+
+
+export default function App() {
+  const [ot,setOtp] =useState(null)
+  const otp =useOtp(ot,setOtp) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App otp:{otp}</div>
+  )
 }
-
-export default App;
