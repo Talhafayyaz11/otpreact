9d87f23a563052d15804fa91237e3a110d9ba208
Talhafayyaz11 <talha@gems.techverx.com>
2022-02-24 19:04:44 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,43 +1,67 @@
 import React from "react";
+import { useReadOTP } from "react-read-otp";
 
-export default class App extends React.Component {
-  state = {
-    otp: ""
-  };
+function App() {
+  const [enabled, setEnabled] = React.useState(false);
+  const [otp, setOTP] = React.useState('');
+  useReadOTP(setOTP, {
+      enabled
+  });
 
-componentDidMount() {
-
-if ("OTPCredential" in window) {
-  const ac = new AbortController();
-
-  navigator.credentials
-    .get({
-      otp: { transport: ["sms"] },
-      signal: ac.signal
-    })
-    .then((ot) => {
-      if(this.state.otp)
-      {
-        this.setState({ otp: null });
-      }
-      this.setState({ otp: ot.code });
-      ac.abort();
-    })
-    .catch((err) => {
-      ac.abort();
-      console.log(err);
-    });
-}
-
-
-}
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello OTP TEST</h1>
-        <h2>Your OTP is: {this.state.otp}</h2>
-      </div>
-    );
+  const handleSendOtp = () => {
+      // do your api call
+      // enable otp listener
+      setEnabled(true);
   }
+  return (
+      <div>
+          <h3>
+              Welcome home 
+              otp:{otp}
+          </h3>
+          <button onClick={handleSendOtp}>send otp</button>
+          <input placeholder="Enter otp" value={otp} onChange={e => setOTP(e.target.value)} />
+      </div>
+  )
 }
+// export default class App extends React.Component {
+//   state = {
+//     otp: ""
+//   };
+
+// componentDidMount() {
+
+// if ("OTPCredential" in window) {
+//   const ac = new AbortController();
+
+//   navigator.credentials
+//     .get({
+//       otp: { transport: ["sms"] },
+//       signal: ac.signal
+//     })
+//     .then((ot) => {
+//       if(this.state.otp)
+//       {
+//         this.setState({ otp: null });
+//       }
+//       this.setState({ otp: ot.code });
+//       ac.abort();
+//     })
+//     .catch((err) => {
+//       ac.abort();
+//       console.log(err);
+//     });
+// }
+
+
+// }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello OTP TEST</h1>
+//         <h2>Your OTP is: {this.state.otp}</h2>
+//       </div>
+//     );
+//   }
+// }
